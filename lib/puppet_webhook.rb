281d26e623e891760676a25916a9fbaf50a8ed1b
Alexander Fisher <alex@linfratech.co.uk>
2017-11-02 09:52:22 +0000

    Rubocop: Fix Layout/AlignParameters

--- lib/puppet_webhook.rb
+++ lib/puppet_webhook.rb
@@ -6,8 +6,8 @@ require 'parsers/webhook_json_parser'
 
 class PuppetWebhook < Sinatra::Base
   use Rack::Parser,
-    parsers: { 'application/json' => Sinatra::Parsers::WebhookJsonParser.new },
-    handlers: { 'application/json' => proc { |e, type| [400, { 'Content-Type' => type }, [{ error: e.to_s }.to_json]] } }
+      parsers: { 'application/json' => Sinatra::Parsers::WebhookJsonParser.new },
+      handlers: { 'application/json' => proc { |e, type| [400, { 'Content-Type' => type }, [{ error: e.to_s }.to_json]] } }
   register Sinatra::ConfigFile
   config_file '../config.yml'
 
