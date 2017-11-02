40d3e7869823ded2110f64017e885fd6635b7891
Alexander Fisher <alex@linfratech.co.uk>
2017-11-02 15:09:02 +0000

    Move repo_user/name parsing to webhook_json_parser

--- lib/helpers/data_parsers.rb
+++ lib/helpers/data_parsers.rb
@@ -15,19 +15,4 @@ module DataParsers # rubocop:disable Style/Documentation
     signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), settings.github_secret, payload_body)
     throw(:halt, [500, "Signatures didn't match!\n"]) unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
-
-  # rubocop:disable Style/RescueModifier
-  # TODO: Move the code from these methods to our parser class
-  def repo_name(data)
-    # Only tested with Github
-    # TODO: Extend for Bitbucket, Bitbucket Server, and Gitlab
-    data['repository']['name'] rescue nil
-  end
-
-  def repo_user(data)
-    # Only tested with Github
-    # TODO: Extend for Bitbucket, Bitbucket Server, and Gitlab
-    data['repository']['owner']['login'] rescue nil
-  end
-  # rubocop:enable Style/RescueModifier
 end
