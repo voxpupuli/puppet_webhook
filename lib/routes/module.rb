5093e2fdd7f7b1709be0cf51a539dae835c15339
David Hollinger <david.hollinger@moduletux.com>
2017-11-19 14:49:49 -0600

    Moved all routes into a routes directory

---
+++ lib/routes/module.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+
+module Sinatra
+  module PuppetWebhookRoutes
+    module Module
+      def self.registered(puppet_webhook)
+        puppet_webhook.post '/module' do
+          protected! if settings.protected
+          request.body.rewind # in case someone has already read it
+
+          # Short circuit if we're ignoring this event
+          return 200 if ignore_event?
+
+          # TODO: Move these two lines of code into the parser
+          decoded = request.body.read
+          verify_signature(decoded) if verify_signature?
+
+          module_name = params['module_name']
+
+          module_name = sanitize_input(module_name)
+          LOGGER.info("Deploying module #{module_name}")
+          deploy_module(module_name)
+        end
+      end
+    end
+  end
+end
