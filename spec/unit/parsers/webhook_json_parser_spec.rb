764ea497040d00c6f628d12ecc95a8c8509a1473
David Hollinger <david.hollinger@nttsecurity.com>
2017-11-21 16:33:07 -0600

    Update tests to mock results of detect_vcs

--- spec/unit/parsers/webhook_json_parser_spec.rb
+++ spec/unit/parsers/webhook_json_parser_spec.rb
@@ -9,6 +9,7 @@ describe Sinatra::Parsers::WebhookJsonParser do
         context 'payload is an update' do
           let(:payload) { read_fixture("#{service}/update.json") }
           it 'returns correctly populated params hash' do
+            expect(subject).to receive(:detect_vcs).and_return(service)
             expect(result).to include(
               branch: 'feature_branch',
               deleted: false,
@@ -18,12 +19,14 @@ describe Sinatra::Parsers::WebhookJsonParser do
           end
           it 'does not populate repo_user', unless: service =~ %r{github|gitlab} do
             # The stash payload doesn't contain anything we could use for repo_user
+            expect(subject).to receive(:detect_vcs).and_return(service)
             expect(result).not_to include(:repo_user)
           end
         end
         context 'payload is for a delete' do
           let(:payload) { read_fixture("#{service}/delete.json") }
           it 'returns params hash with :deleted => true' do
+            expect(subject).to receive(:detect_vcs).and_return(service)
             expect(result).to include(deleted: true)
           end
         end
