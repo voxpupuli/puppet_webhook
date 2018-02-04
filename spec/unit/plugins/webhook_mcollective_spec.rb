ab2c7813d65078fb620b40e172660c101e1baa5c
David Hollinger <david.hollinger@moduletux.com>
2018-02-03 20:29:32 -0600

    Add initial test

--- spec/unit/plugins/webhook_mcollective_spec.rb
+++ spec/unit/plugins/webhook_mcollective_spec.rb
@@ -3,6 +3,9 @@ require 'mcollective'
 
 describe PuppetWebhook::Mcollective do
   describe '#run' do
+    before(:each) do
+      allow(PuppetWebhook::Mcollective).to receive(:new).and_return(nil)
+    end
     it 'should run correctly' do
       result_data = {
         :agent => 'puppet',
@@ -15,6 +18,9 @@ describe PuppetWebhook::Mcollective do
             :enabled => false
         }
       }
+
+      allow(PuppetWebhook::Mcollective).to receive(:run).and_return(result_data)
+      expect(PuppetWebhook::Mcollective.run).to eq(result_data)
     end
   end
 end
