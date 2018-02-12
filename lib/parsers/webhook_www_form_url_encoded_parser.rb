c526feeccd1f0851f07852d7333b28a136c0e0cc
Will Berman <wlbberman@gmail.com>
2018-02-12 01:18:08 +0000

    added url encoded parser

--- lib/parsers/webhook_www_form_url_encoded_parser.rb
+++ lib/parsers/webhook_www_form_url_encoded_parser.rb
@@ -3,13 +3,15 @@ require 'json'
 
 require_relative 'webhook_parser'
 
+require 'byebug'
+
 module Sinatra
   module Parsers
-    class WebhookJsonParser # rubocop:disable Style/Documentation
+    class WebhookWWWFormURLEncodedParser
       include Sinatra::Parsers::WebhookParser
 
       def parse_data(body)
-        JSON.parse(body, quirks_mode: true)
+        JSON.parse(CGI.unescape(body).gsub(%r{^payload\=}, ''))
       end
     end
   end
