0a6b5a9b61f619e48fbe2b70aed7073789196b77
David Hollinger <david.hollinger@moduletux.com>
2020-08-30 13:43:36 -0500

    More build process fixes

--- resources/postinst.sh
+++ resources/postinst.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export WEBHOOK_CONFDIR=/etc/voxpupuli/webhook.yaml
 export RACK_ENV=production
 export RUBYLIB=/opt/voxpupuli/webhook/lib/ruby:/opt/voxpupuli/webhook
 export GEM_HOME=/opt/voxpupuli/webhook/lib/ruby/gems/2.6.0
@@ -6,6 +7,6 @@ export GEM_PATH=$GEM_HOME
 export LD_PATH=/opt/voxpupuli/webhook/lib
 export PATH=/opt/voxpupuli/webhook/bin:$PATH
 
-cd /opt/voxpupuli/webhook && bin/bundle install --with default > /dev/null 2>&1 && bin/bundle exec rake db:migrate > /dev/null 2>&1
+cd /opt/voxpupuli/webhook && bin/bundle install --with default --quiet && bin/bundle exec rake db:migrate --quiet
 
 echo "Redis is required to start Puppet Webhook. Please install and start Redis first."
